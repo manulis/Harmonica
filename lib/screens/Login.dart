0524736eb17b07c90dd187eeb6e041bf8c16cb79
manulis <manulisdesable@gmail.com>
2024-01-15 17:23:34 +0100

    Login apartado visual version 0

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,37 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:harmonica/widgets.dart';
 
 class Login extends StatefulWidget{
   @override
   State<Login> createState() => _Login();
 }
 
-
 class _Login extends State<Login>{
   Widget build(BuildContext context){
-  final height = MediaQuery.of(context).size.height ;
   final width = MediaQuery.of(context).size.width ;
+  final height = MediaQuery.of(context).size.height ;
     return Scaffold(
       backgroundColor: Color.fromRGBO(59, 6, 69, 1),
       body: SizedBox(
          height: height,
          child: Stack(children: [
-            Container(
-              height: height/2,
-              width: double.infinity,
-              decoration:
-              const BoxDecoration(
-                color: Color.fromRGBO(40, 4, 64, 1),
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(50),
-                  bottomLeft: Radius.circular(50)
-                )
-              ),
-              child:  Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset("assets/images/LogoPositivo.png", width: 300,),
-              ]),
-            ),
+            headLogo(height),
             SingleChildScrollView(
               child: SizedBox(
                 height: height,
@@ -51,8 +35,32 @@ class _Login extends State<Login>{
                         child: Column( 
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                            const Text('Login', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
                             const SizedBox(height: 22),
+                            const Text('Login', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+                            const SizedBox(height: 30),
+                            TextField(
+                              decoration: const InputDecoration(
+                                labelText: 'Name', labelStyle: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontStyle: FontStyle.italic),
+                                hintText: 'Write your name' ,hintStyle: TextStyle(color: Color.fromARGB(255, 172, 172, 172), fontStyle: FontStyle.italic),
+                              ),
+                                onChanged: (text) {
+                                setState(() {
+
+                                });
+                              },
+                            ),
+
+                            PasswordTextField(),
+
+                            const SizedBox(height: 30),
+
+                            buildButton('Sign In', () { 
+
+
+                            })
+
+
+
                         ],
                         ),
                     )
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -24,6 +24,28 @@ class _TitlesState extends State<Titles> {
   }
 }
 
+
+//Cabecera con Logo
+Widget headLogo(double height){
+  return Container(
+     height: height/2,
+          width: double.infinity,
+          decoration:
+          const BoxDecoration(
+            color: Color.fromRGBO(40, 4, 64, 1),
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(50),
+              bottomLeft: Radius.circular(50)
+            )
+          ),
+          child:  Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/LogoPositivo.png", width: 300,),
+          ]),
+  );
+}
+
 //Widgets para botones
 Widget buildButton(String text, VoidCallback onPressed) {
   return TextButton(
@@ -36,4 +58,35 @@ Widget buildButton(String text, VoidCallback onPressed) {
     ),
     child: Text(text),
   );
+}
+
+//campos de texto especiales
+
+class PasswordTextField extends StatefulWidget {
+  @override
+  _PasswordTextFieldState createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  bool _obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextField(
+        obscureText: _obscureText,
+        decoration: InputDecoration(
+          labelText: 'Password', labelStyle: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontStyle: FontStyle.italic),
+                                hintText: 'Write your Password' ,hintStyle: TextStyle(color: Color.fromARGB(255, 172, 172, 172), fontStyle: FontStyle.italic),
+          suffixIcon: IconButton(
+            icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+            onPressed: () {
+              setState(() {
+                _obscureText = !_obscureText;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
