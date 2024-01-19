fad0377bfa6958989cade933fc88a026f8770183
manulis <manulisdesable@gmail.com>
2024-01-19 12:24:48 +0100

    Validación de telefono, email y campos vacios

--- lib/functions/validateFormsCamps.dart
+++ lib/functions/validateFormsCamps.dart
@@ -1,18 +1,41 @@
 import 'package:email_validator/email_validator.dart';
 
+//Funciones de validación
+
+bool emailValidate(String? email){
+  bool isValid = EmailValidator.validate(email!);
+  return isValid;
+}
+
+bool phoneValidate(String? phone){
+  if(phone == ''){
+    return true;
+  }
+  RegExp regExp = RegExp(r'^\d{9}$');
+  return regExp.hasMatch(phone!);
+}
 
 
+//Funciones que en el caso de que un parametro no sea valido o este vacio lo indique en el formulario
+String?  FocusEmail(String? value){
+  if(value == ''){
+    return 'The field is required';
+  }
+  if(!emailValidate(value)){
+    return 'Email invalido';
+  }
+  return null;
+}
 
-bool emailValidate(String email){
-  if(email.isEmpty){
-    return false;
-  }else{
-    bool isValid = EmailValidator.validate(email);
-    return isValid;
+String? FocusPhone(String? value){
+  if(!phoneValidate(value)){
+    return 'Telefono invalido';
   }
 }
 
-bool phoneValidate(String phone){
-  RegExp regExp = RegExp(r'^\+34\s\d{3}\s\d{3}\s\d{3}$');
-  return regExp.hasMatch(phone);
+String? FocusName(String? value) {
+  if (value == null || value.isEmpty){
+    return 'The field is required';
+  }
+  return null;
 }
--- lib/screens/Register.dart
+++ lib/screens/Register.dart
@@ -3,8 +3,8 @@ import 'package:harmonica/functions/validateFormsCamps.dart';
 import 'package:harmonica/widgets.dart';
 import 'package:harmonica/navigator.dart';
 import 'package:harmonica/functions/databasePetitions.dart';
-import 'package:intl/intl.dart'; // Añadido para el formato de fecha
-import 'package:harmonica/objects/User.dart' as UserObject; // Añadido para el manejo de objetos de usuario
+import 'package:intl/intl.dart';
+import 'package:harmonica/objects/User.dart' as UserObject;
 
 class Register extends StatefulWidget {
   @override
@@ -19,6 +19,10 @@ class _Register extends State<Register> {
   String email = '';
   String phone = '';
   String password = '';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _NameController = TextEditingController();
+  final TextEditingController _EmailController = TextEditingController();
+  final TextEditingController _PhoneController = TextEditingController();
 
   void _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
@@ -36,7 +40,6 @@ class _Register extends State<Register> {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
       backgroundColor: Color.fromRGBO(40, 4, 64, 1),
       body: SafeArea(
@@ -68,7 +71,7 @@ class _Register extends State<Register> {
                       borderRadius: BorderRadius.circular(32),
                     ),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                     
                       children: [
                         const SizedBox(height: 20),
                         Row(
@@ -89,163 +92,181 @@ class _Register extends State<Register> {
                             const SizedBox(width: 50),
                           ],
                         ),
-                        const SizedBox(height: 15),
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: 'User Name',
-                            labelStyle: TextStyle(
-                                color: Color.fromARGB(255, 0, 0, 0),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                            hintText: 'Write your name',
-                            hintStyle: TextStyle(
-                                color: Color.fromARGB(255, 172, 172, 172),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                          ),
-                          onChanged: (text) {
-                            setState(() {
-                              name = text;
-                            });
-                          },
-                        ),
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: 'Email',
-                            labelStyle: TextStyle(
-                                color: Color.fromARGB(255, 0, 0, 0),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                            hintText: 'Write your Email',
-                            suffixIcon: Icon(Icons.email),
-                            hintStyle: TextStyle(
-                                color: Color.fromARGB(255, 172, 172, 172),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                          ),
-                          onChanged: (text) {
-                            setState(() {
-                              email = text;
-                            });
-                          },
-                        ),
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: 'Phone number',
-                            labelStyle: TextStyle(
-                                color: Color.fromARGB(255, 0, 0, 0),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                            hintText: '+34',
-                            suffixIcon: Icon(Icons.phone),
-                            hintStyle: TextStyle(
-                                color: Color.fromARGB(255, 172, 172, 172),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                          ),
-                          onChanged: (text) {
-                            setState(() {
-                              phone = '+34 ' + text;
-                            });
-                          },
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            _selectDate(context);
-                          },
-                          child: InputDecorator(
-                            decoration: const InputDecoration(
-                              suffixIcon: Icon(Icons.calendar_today),
-                            ),
-                            child: Text(
-                              DateFormat('dd/MM/yyyy').format(_selectedDate),
-                              style: const TextStyle(fontSize: 16.0),
-                            ),
-                          ),
-                        ),
-                        TextField(
-                          onChanged: (text) {
-                            setState(() {
-                              password = text;
-                            });
-                          },
-                          obscureText: _obscureText1,
-                          decoration: InputDecoration(
-                            labelText: 'Create Password',
-                            labelStyle: const TextStyle(
-                                color: Color.fromARGB(255, 0, 0, 0),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                            hintText: 'Type a Password',
-                            hintStyle: const TextStyle(
-                                color: Color.fromARGB(255, 172, 172, 172),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                            suffixIcon: IconButton(
-                              icon: Icon(_obscureText1
-                                  ? Icons.visibility
-                                  : Icons.visibility_off),
-                              onPressed: () {
-                                setState(() {
-                                  _obscureText1 = !_obscureText1;
-                                });
-                              },
-                            ),
-                          ),
-                        ),
-                        TextField(
-                          onChanged: (text) {
-                            setState(() {});
-                          },
-                          obscureText: _obscureText2,
-                          decoration: InputDecoration(
-                            labelText: 'Confirm Password',
-                            labelStyle: const TextStyle(
-                                color: Color.fromARGB(255, 0, 0, 0),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                            hintText: 'Retype your Password',
-                            hintStyle: const TextStyle(
-                                color: Color.fromARGB(255, 172, 172, 172),
-                                fontStyle: FontStyle.italic,
-                                fontSize: 16),
-                            suffixIcon: IconButton(
-                              icon: Icon(_obscureText2
-                                  ? Icons.visibility
-                                  : Icons.visibility_off),
-                              onPressed: () {
-                                setState(() {
-                                  _obscureText2 = !_obscureText2;
-                                });
-                              },
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 30),
-                        SizedBox(
-                          child: buildButton('Sign Up', () async {
-                            String birthDate = DateFormat('dd/MM/yyyy').format(_selectedDate);
-                            UserObject.User user = UserObject.User(name, email, phone, birthDate, password, [''], ['']);
-                            if(emailValidate(email) && phoneValidate(phone)){
-                              print(user);
-                              bool registerInfo = await userHandler.postUser(user);
-                              if (registerInfo) {
-                                PopUp(context, 'Registrado', 'Registrado con exito');
-                              } else {
-                                PopUp(context, 'Error', 'Parece que hubo un error');
-                              }
-                            }
-                          
-                          }),
-                        ),
-                        const SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                        Form(
+                        key: _formKey,
+                        child:
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
                           children: [
-                            buildClickableText('Already have an account? Sign In', () {
-                              nav('Login', context);
-                            }),
-                          ],
+                            const SizedBox(height: 15),
+                            TextFormField(
+                              controller: _NameController,
+                              validator: FocusName,
+                              decoration:  const InputDecoration(
+                                labelText: 'User Name',
+                                labelStyle:  TextStyle(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                                hintText: 'Write your name',
+                                hintStyle:  TextStyle(
+                                    color: Color.fromARGB(255, 172, 172, 172),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                              ),
+                              onChanged: (text) {
+                                setState(() {
+                                  name = text;
+                                });
+                              },
+                            ),
+                            TextFormField(
+                              controller: _EmailController,
+                              validator: FocusEmail,
+                              decoration: const InputDecoration(
+                                
+                                labelText: 'Email',
+                                labelStyle: TextStyle(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                                hintText: 'Write your Email',
+                                suffixIcon: Icon(Icons.email),
+                                hintStyle: TextStyle(
+                                    color: Color.fromARGB(255, 172, 172, 172),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                              ),
+                              onChanged: (text) {
+                                setState(() {
+                                  email = text;
+                                });
+                              },
+                            ),
+                            TextFormField(
+                              controller: _PhoneController,
+                              validator: FocusPhone,
+                              decoration: const InputDecoration(
+                                labelText: 'Phone number',
+                                labelStyle: TextStyle(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                                hintText: 'Write your Phone number',
+                                suffixIcon: Icon(Icons.phone),
+                                hintStyle: TextStyle(
+                                    color: Color.fromARGB(255, 172, 172, 172),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                              ),
+                              onChanged: (text) {
+                                setState(() {
+                                  phone = text;
+                                });
+                              },
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                _selectDate(context);
+                              },
+                              child: InputDecorator( 
+                                decoration: const InputDecoration(
+                                  labelText: 'Birth Date',
+                                  suffixIcon: Icon(Icons.calendar_today),
+                                ),
+                                child: Text(
+                                  DateFormat('dd/MM/yyyy').format(_selectedDate),
+                                  style: const TextStyle(fontSize: 16.0),
+                                ),
+                              ),
+                            ),
+                            TextFormField(
+                              onChanged: (text) {
+                                setState(() {
+                                  password = text;
+                                });
+                              },
+                              obscureText: _obscureText1,
+                              decoration: InputDecoration(
+                                labelText: 'Create Password',
+                                labelStyle: const TextStyle(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                                hintText: 'Type a Password',
+                                hintStyle: const TextStyle(
+                                    color: Color.fromARGB(255, 172, 172, 172),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                                suffixIcon: IconButton(
+                                  icon: Icon(_obscureText1
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                  onPressed: () {
+                                    setState(() {
+                                      _obscureText1 = !_obscureText1;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                            TextFormField(
+                              onChanged: (text) {
+                                setState(() {});
+                              },
+                              obscureText: _obscureText2,
+                              decoration: InputDecoration(
+                                labelText: 'Confirm Password',
+                                labelStyle: const TextStyle(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                                hintText: 'Retype your Password',
+                                hintStyle: const TextStyle(
+                                    color: Color.fromARGB(255, 172, 172, 172),
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 16),
+                                suffixIcon: IconButton(
+                                  icon: Icon(_obscureText2
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                  onPressed: () {
+                                    setState(() {
+                                      _obscureText2 = !_obscureText2;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 30),
+                            SizedBox(
+                              child: buildButton('Sign Up', () async {
+                                _save();
+                                String birthDate = DateFormat('dd/MM/yyyy').format(_selectedDate);
+                                UserObject.User user = UserObject.User(name, email, phone, birthDate, password, [''], ['']);
+                                print(email);
+                                if(emailValidate(email) && phoneValidate(user.phone) && name.isNotEmpty && password.isNotEmpty){
+                                  print(user);
+                                  bool registerInfo = await userHandler.postUser(user);
+                                  if (registerInfo) {
+                                    PopUp(context, 'Registrado', 'Registrado con exito');
+                                  }
+                                  if(!registerInfo) {
+                                    PopUp(context, 'Error', 'Parece que hubo un error');
+                                  }
+                                }
+                              }),
+                            ),
+                            const SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                buildClickableText('Already have an account? Sign In', () {
+                                  nav('Login', context);
+                                }),
+                              ],
+                            ),
+                          ],)
                         ),
                       ],
                     ),
@@ -258,4 +279,10 @@ class _Register extends State<Register> {
       ),
     );
   }
+
+  void _save() {
+    if (_formKey.currentState!.validate()) print('Okey');
+    print('Error');
+  }
+
 }
